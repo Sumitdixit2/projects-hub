8267bfb4edcd9f60d7eceeb2aba301256d70b038
Sumitdixit2 <sumitdixit00007@gmail.com>
2026-05-02 07:19:56 +0000

    activity api created

--- apps/api/src/routes/activity/activity.routes.ts
+++ apps/api/src/routes/activity/activity.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { verifyJWT } from "../../middlewares/verifyJwt";
 import { isMyId } from "../../middlewares/isMyId.middleware";
-import { getMyActivity } from "../../controllers/activity/activity.controller";
+import { getAdminActivity, getMyActivity } from "../../controllers/activity/activity.controller";
+import { requireAdmin } from "../../middlewares/validateUser.middleware";
+import { validateAdmin } from "../../middlewares/validate.middleware";
 
 const activityRouter = Router();
 
-activityRouter.route('/getActivity').get(verifyJWT,isMyId,getMyActivity);
+activityRouter.route('/getActivity/me').get(verifyJWT,isMyId,getMyActivity);
+activityRouter.route('/getActivity/:id').get(verifyJWT,requireAdmin,validateAdmin,getAdminActivity);
 
 export default activityRouter;
