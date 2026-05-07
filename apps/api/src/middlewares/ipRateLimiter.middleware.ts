296dc7dabdaa15efd31ec7eae684c8965be84eeb
Sumitdixit2 <sumitdixit00007@gmail.com>
2026-05-07 12:25:16 +0000

    ipRateLimiter created to use

---
+++ apps/api/src/middlewares/ipRateLimiter.middleware.ts
@@ -0,0 +1,15 @@
+import rateLimit from "express-rate-limit";
+import RedisStore from "rate-limit-redis";
+import { client } from "../config/client";
+
+const ipLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  store: new RedisStore({
+		sendCommand: (...args: string[]) => client.sendCommand(args),
+	}),
+});
+
+export {ipLimiter};
